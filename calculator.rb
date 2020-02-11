44c7f42601ac3d4950c61f5702e9c85008a152c0
Vanessa Martinez <vmar76@gmail.com>
2020-02-11 18:15:28 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,10 @@
-# Add your variables here
+first_number = 3
+second_number = 4
+sum = first_number + second_number
+puts sum 
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
